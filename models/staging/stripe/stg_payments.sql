709a3457cc7a30c1d15e44f262f78c891e4d6779
samira.kalwari@gmail.com <samira.kalwari@gmail.com>
2023-07-20 06:37:05 +0000

    updated

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,3 +1,6 @@
+{{ config(required_tests=None) }}
+
+
 select
     id as payment_id,
     orderid as order_id,
@@ -5,7 +8,8 @@ select
     status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    -- amount / 100 as amount,
+    {{ cents_to_dollors('amount', 3)}} as amount,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
